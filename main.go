0658c022fe85e85eeda7ce744437df22a08a686a
KMimura <mimurakohei256@gmail.com>
2021-12-14 19:51:11 +0900

    hello world

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fyne.io/fyne/app"
+    "fyne.io/fyne/widget"
+)
+
+func main() {
+    myApp := app.New()
+    myWindow := myApp.NewWindow("Hello")
+    myWindow.SetContent(widget.NewLabel("Hello World!"))
+    myWindow.ShowAndRun()
+}
