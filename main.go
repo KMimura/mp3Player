0e51422644a0aef1accc40676d83417eb9b2409e
KMimura <mimurakohei256@gmail.com>
2021-12-15 09:57:54 +0900

    resize the window

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-    "fyne.io/fyne/app"
-    "fyne.io/fyne/widget"
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
 )
 
 func main() {
-    myApp := app.New()
-    myWindow := myApp.NewWindow("Hello")
-    myWindow.SetContent(widget.NewLabel("Hello World!"))
-    myWindow.ShowAndRun()
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Hello")
+	myWindow.Resize(fyne.NewSize(255, 510))
+	myWindow.SetContent(widget.NewLabel("Hello World!"))
+	myWindow.ShowAndRun()
 }
