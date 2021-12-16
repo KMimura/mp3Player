fe05e912a8e6844a4db01610af07e559e0751f49
KMimura <mimurakohei256@gmail.com>
2021-12-16 23:25:55 +0900

    add a button

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	// "image/color"
+
+	"fmt"
+
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
 
+// var grey = &color.Gray{Y: 123}
+
 func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Hello")
+	myWindow.SetContent(widget.NewButton("aaa", func() {
+		fmt.Println("aaa")
+	}))
 	myWindow.Resize(fyne.NewSize(255, 510))
-	myWindow.SetContent(widget.NewLabel("Hello World!"))
+	// myWindow.SetContent(widget.NewLabel("Hello World!"))
 	myWindow.ShowAndRun()
 }
