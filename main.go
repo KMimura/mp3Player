dc8669e293cba13e79a72de4edf92391c4f19b42
KMimura <mimurakohei256@gmail.com>
2021-12-18 15:29:19 +0900

    show multiple items on a window

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -15,13 +16,14 @@ func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Hello")
 	hello := widget.NewLabel("Hello Fyne!")
-	myWindow.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("aaa", func() {
-			hello.SetText("aaa")
-			ch := make(chan string)
-			go showAnother(myApp, ch)
-		})))
+	mainLabel := widget.NewLabel("Hello")
+	button := widget.NewButton("aaa", func() {
+		hello.SetText("aaa")
+		ch := make(chan string)
+		go showAnother(myApp, ch)
+	})
+	content := container.New(layout.NewVBoxLayout(), mainLabel, button)
+	myWindow.SetContent(content)
 	myWindow.Resize(fyne.NewSize(255, 510))
 	// myWindow.SetContent(widget.NewLabel("Hello World!"))
 	myWindow.ShowAndRun()
