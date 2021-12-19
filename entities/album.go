cb62ae1a09efdf80675d6eb8755b5e22bb54cfb5
KMimura <mimurakohei256@gmail.com>
2021-12-20 00:14:57 +0900

    add entities

--- entities/album.go
+++ entities/album.go
@@ -3,4 +3,5 @@ package entities
 type Album struct {
 	AlbumTitle  string
 	AlbumArtist string
+	AlbumCover  coverArt
 }
---
+++ entities/coverArt.go
@@ -0,0 +1,5 @@
+package entities
+
+type coverArt struct {
+	Location string
+}
