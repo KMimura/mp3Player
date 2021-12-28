31bd5a6b32e6f39981a6a23f4293ff1f79752c4f
KMimura <mimurakohei256@gmail.com>
2021-12-28 22:31:05 +0900

    add color to the buttons

--- widgets/mainFrame.go
+++ widgets/mainFrame.go
@@ -36,12 +36,18 @@ func createButtons() *fyne.Container {
 	leftButton := widget.NewButton("", func() {
 		fmt.Println("left")
 	})
+	leftButtonSheet := canvas.NewRectangle(color.White)
+	leftButtonContainer := container.New(layout.NewMaxLayout(), leftButton, leftButtonSheet)
 	centerButton := widget.NewButton("", func() {
 		fmt.Println("center")
 	})
+	centerButtonSheet := canvas.NewRectangle(color.White)
+	centerButtonContainer := container.New(layout.NewMaxLayout(), centerButton, centerButtonSheet)
 	rightButton := widget.NewButton("", func() {
 		fmt.Println("right")
 	})
-	buttonsContainer := container.New(layout.NewHBoxLayout(), leftButton, centerButton, rightButton)
+	rightButtonSheet := canvas.NewRectangle(color.White)
+	rightButtonContainer := container.New(layout.NewMaxLayout(), rightButton, rightButtonSheet)
+	buttonsContainer := container.New(layout.NewHBoxLayout(), leftButtonContainer, centerButtonContainer, rightButtonContainer)
 	return buttonsContainer
 }
