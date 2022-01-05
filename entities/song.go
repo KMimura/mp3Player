7fb3b9f554d793cc01d5ee0ec1e8f2cc15eef4e7
KMimura <mimurakohei256@gmail.com>
2022-01-05 13:35:01 +0900

    add functions to song struct

--- entities/song.go
+++ entities/song.go
@@ -1,9 +1,21 @@
 package entities
 
+import (
+	"fmt"
+)
+
 type Song struct {
 	SongName     string
 	SongLocation string
 	Artist       string
 	Album        Album
 	Length       Artist
+}
+
+func (*Song) Play() {
+	fmt.Println("play")
+}
+
+func (*Song) Stop() {
+	fmt.Println("stop")
 }
