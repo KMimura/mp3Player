9345bb8d2f860262e11092c1aaf047cca9298e16
KMimura <mimurakohei256@gmail.com>
2022-01-13 21:29:05 +0900

    add test impl for fileManger

--- interfaceAdapters/testFileMangerImpl.go
+++ interfaceAdapters/testFileMangerImpl.go
@@ -5,5 +5,8 @@ type TestFileManagerImpl struct {
 }
 
 func (*TestFileManagerImpl) GetSongsFromPath(path string) []string {
-	return nil
+	sampleOne := "../sampleSounds/sample_song.mp3"
+	sampleSongs := make([]string, 1)
+	sampleSongs[0] = sampleOne
+	return sampleSongs
 }
