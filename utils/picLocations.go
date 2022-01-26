b4cb37d58050ccbc13bf208ff612b32e94ee62ab
KMimura <mimurakohei256@gmail.com>
2022-01-26 18:25:35 +0900

    add utils

---
+++ utils/picLocations.go
@@ -0,0 +1,9 @@
+package utils
+
+// pics locations relative to the "app" folder
+const (
+	RightArrowPic  = "pics/right.png"
+	LeftArrowPic   = "pics/left.png"
+	StartButtonPic = "pics/start.png"
+	StopButtonPic  = "pics/stop.png"
+)
