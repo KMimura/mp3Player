c1c59600ae4c68318007d93a44ba27d4f0990bf2
KMimura <mimurakohei256@gmail.com>
2022-01-28 14:35:19 +0900

    implement GetSongPathes

--- interfaceAdapters/playOrderManagerImpl.go
+++ interfaceAdapters/playOrderManagerImpl.go
@@ -1,13 +1,44 @@
 package interfaceAdapters
 
+import (
+	"io/ioutil"
+	"strings"
+)
+
+var acceptedExtentions = []string{"mp3"}
+
 type PlayOrderManagerImpl struct {
 	queue []string
 	index int
 }
 
 func (pom *PlayOrderManagerImpl) GetSongPathes(path string) []string {
+	var filePathes []string
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return filePathes
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		fileNameSplitted := strings.Split(file.Name(), ".")
+		if len(fileNameSplitted) <= 1 {
+			continue
+		}
+		var acceptanceOnExtentionFlg bool
+		for _, e := range acceptedExtentions {
+			if fileNameSplitted[len(fileNameSplitted)-1] == e {
+				acceptanceOnExtentionFlg = true
+				break
+			}
+		}
+		if acceptanceOnExtentionFlg {
+			filePathes = append(filePathes, file.Name())
+		}
+	}
 	pom.index = 0
-	return nil
+	return filePathes
 }
 
 func (pom *PlayOrderManagerImpl) UpdateIndex(index int) {
