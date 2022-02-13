4eaae2ab8160ed06bee8c40a4ac1e38b66a5f443
KMimura <mimurakohei256@gmail.com>
2022-02-13 14:38:53 +0900

    make a blueprint for uci

--- useCases/useCasesInteractor.go
+++ useCases/useCasesInteractor.go
@@ -6,6 +6,28 @@ type UseCasesInteractors struct {
 	sp  SongPlayer
 }
 
+func (uci *UseCasesInteractors) Play() {
+	// get the current song from pom
+	// get the song file from fm
+	// play the song with sp
+}
+
+func (uci *UseCasesInteractors) Next() {
+	// if currently playing, call the pause func
+	// ask pom for the next song
+	// get the song from fm
+	// play the song with sp
+}
+
+func (uci *UseCasesInteractors) Pause() {
+	// pause the song with sp
+}
+
+func (uci *UseCasesInteractors) UpdateQueue() {
+	// update pom
+	// call play func
+}
+
 func NewUseCasesInteractors(fm FileManager, pom PlayOrderManager, sp SongPlayer) *UseCasesInteractors {
 	uci := &UseCasesInteractors{fm, pom, sp}
 	return uci
